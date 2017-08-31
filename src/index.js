65d7e612f25121eb6acebbb3a26b2710eab6311a
henry <calren00@gmail.com>
2017-08-31 10:35:40 +0900

    redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//Router
 import { BrowserRouter, Route } from 'react-router-dom'
+//Container Component
 import { App, Home, Login, Register } from 'containers';
+
+//Redux
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import reducers from 'reducers';
+import thunk from 'redux-thunk';
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
+    <Provider store={store}>
         <BrowserRouter>
                 <App>
                     <Route exact path="/" component={Home}/>
@@ -12,5 +25,6 @@ ReactDOM.render(
                     <Route path="login" component={Login}/>
                     <Route path="register" component={Register}/>
                 </App>
-        </BrowserRouter>, rootElement
+        </BrowserRouter>
+    </Provider>,rootElement
 );
